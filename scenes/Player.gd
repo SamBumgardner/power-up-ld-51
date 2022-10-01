c10ac223fb5e86eb30a115f35dd5a00c15449fe3
Sam Bumgardner <Sambumgardner1@gmail.com>
2022-10-01 12:17:44 -0500

    Add Bullet, test collision

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -16,14 +16,14 @@ func _ready():
 	max_x = screen_size.x - radius
 	max_y = screen_size.y - radius
 
-func _process(delta):
+func _process(_delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed(player_prefix + "move_right"):
 		velocity.x += 1
