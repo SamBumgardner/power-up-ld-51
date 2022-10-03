49844e6f97b01c06efd7bf799cb151d42d3fd066
Sam Bumgardner <Sambumgardner1@gmail.com>
2022-10-02 22:12:38 -0500

    color-code turrets

--- scenes/Turret.gd
+++ scenes/Turret.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 class_name Turret
 
-func init(_pattern:Node, _position:Vector2):
+const BLUE = Color(.5, .5, 1, 1)
+const ORANGE = Color(1, .5, .6, 1)
+
+func init(_pattern:PatternBase, _position:Vector2):
 	add_child(_pattern)
 	position = _position
+
+	if (_pattern.bullet_collision_mask % 2 == 0):
+		$AnimatedSprite.modulate = BLUE
+	else:
+		$AnimatedSprite.modulate = ORANGE
