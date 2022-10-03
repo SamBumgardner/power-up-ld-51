5aa13a1d437d8724dca8975fadb5e773f24b172d
Sam Bumgardner <Sambumgardner1@gmail.com>
2022-10-02 19:08:54 -0500

    adjusted upgrade progress for visual appeal

--- scenes/UpgradeProgress.gd
+++ scenes/UpgradeProgress.gd
@@ -1,6 +1,12 @@
-extends ProgressBar
+extends Node2D
 
 onready var timer = $UpgradeTimer as Timer
+onready var left_half = $LeftHalf as ProgressBar
+onready var right_half = $RightHalf as ProgressBar
 
 func _process(_delta):
-	value = (timer.wait_time - timer.time_left) / timer.wait_time * 100
+	set_progress(left_half)
+	set_progress(right_half)
+
+func set_progress(bar:ProgressBar):
+	bar.value = (timer.wait_time - timer.time_left) / timer.wait_time * 100
