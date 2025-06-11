f97bdc1649f9bc2ee4d915ce729f746597e227bc
Noah Bumgardner <noah@playgamecafe.com>
2025-06-11 18:22:16 -0500

    Revert "Copy Cannon Base Constructor into Children, Error"
    This reverts commit 30c316d0d9e3b136b766f7ea1d3f33d603a6aecb.

--- patterns/plus/CannonRight.gd
+++ patterns/plus/CannonRight.gd
@@ -4,11 +4,7 @@ class_name CannonRight
 
 func _init(_pattern_parent:Node2D, _pattern_duration:float,
 		_pattern_collision:int, _projectile_infos:Array):
-	pattern_parent = _pattern_parent
-	pattern_duration = _pattern_duration
-	pattern_collision = _pattern_collision
-	
-	projectile_infos = _projectile_infos
+	pass
 
 func populate_velocity(projectile_info:ProjectileInfo):
 	calc_vector = Vector2.RIGHT
--- patterns/targeted/CannonTargeted.gd
+++ patterns/targeted/CannonTargeted.gd
@@ -6,11 +6,6 @@ var target_node:Node2D
 
 func _init(_pattern_parent:Node2D, _pattern_duration:float,
 		_pattern_collision:int, _projectile_infos:Array, _target_node:Node2D):
-	pattern_parent = _pattern_parent
-	pattern_duration = _pattern_duration
-	pattern_collision = _pattern_collision
-	
-	projectile_infos = _projectile_infos
 	target_node = _target_node
 
 func populate_velocity(projectile_info:ProjectileInfo):
