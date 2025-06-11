02bf2ba0c2f2a860087807a5309458a25668a7c9
Noah Bumgardner <noah@playgamecafe.com>
2025-06-11 18:18:03 -0500

    Revert "Log to Find Infinite Fire Cannon Error, Debug Only"
    This reverts commit 3ef4724dea2221c889d206aeb26beaa1ab27130f.

--- patterns/CannonBase.gd
+++ patterns/CannonBase.gd
@@ -14,7 +14,6 @@ var projectile_infos:Array
 var calc_vector:Vector2 = Vector2(1, 0)
 
 func _init(_pattern_parent:Node2D, _pattern_duration:float, _pattern_collision:int, _projectile_infos:Array):
-	print_debug('DEBUG05 initialize cannon with:', _pattern_parent, _pattern_duration, _pattern_collision, _projectile_infos)
 	pattern_parent = _pattern_parent
 	pattern_duration = _pattern_duration
 	pattern_collision = _pattern_collision
@@ -23,28 +22,15 @@ func _init(_pattern_parent:Node2D, _pattern_duration:float, _pattern_collision:i
 	pass
 
 func try_shoot(start_time, end_time):
-	# print_debug('DEBUG06 trying to shoot at time with projectiles:', start_time, projectile_infos)
 	for projectile_info in projectile_infos:
-		# Prints too many times and crashes the game.
-		# print_debug('DEBUG07 projectile checking if it can shoot at:', start_time)
 		if (start_time < projectile_info.shoot_time and end_time > projectile_info.shoot_time) \
 				or (end_time > pattern_duration and fmod(end_time, pattern_duration)  > projectile_info.shoot_time):
-			# Adding a return prevents a crash, likely from an infinite loop error.
-			# Returning early correctly gets reached when a bullet should be fired.
-			# print_debug('DEBUG08 shooting at time:', start_time)
-			# return
+			
 			populate_velocity(projectile_info)
 			
-			# Break out of the loop after firing a projectile.
-			return
-			
-			# Signal `fired_projectile` seems to be able to crash the game.
 			emit_signal("fired_projectile", pattern_parent.global_position, 
 				projectile_info.velocity, pattern_collision,
 				projectile_sprite_frame)
-			# Break out of the loop after firing a projectile.
-			# Too late and crashes the game.
-			# return
 
 func populate_velocity(projectile_info:ProjectileInfo):
 	calc_vector = Vector2.RIGHT
--- patterns/random/CannonRandom.gd
+++ patterns/random/CannonRandom.gd
@@ -2,17 +2,9 @@ extends CannonBase
 
 class_name CannonRandom
 
-#func _init(_pattern_parent:Node2D, _pattern_duration:float,
-#		_pattern_collision:int, _projectile_infos:Array):
-#	pass
-
-#func _init(_pattern_parent:Node2D, _pattern_duration:float,
-#		_pattern_collision:int, _projectile_infos:Array):
-#	print_debug('DEBUG11 initialize cannon with:', _pattern_parent, _pattern_duration, _pattern_collision, _projectile_infos)
-#	# Probably creates an infinite loop with `super` of constructor calls.
-#	#super(_pattern_parent, _pattern_duration,
-#	#	_pattern_collision, _projectile_infos)
-#	pass
+func _init(_pattern_parent:Node2D, _pattern_duration:float,
+		_pattern_collision:int, _projectile_infos:Array):
+	pass
 
 func populate_velocity(projectile_info:ProjectileInfo):
 	calc_vector = Vector2.RIGHT.rotated(randf() * PI * 2)
