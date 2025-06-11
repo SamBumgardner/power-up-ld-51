30c316d0d9e3b136b766f7ea1d3f33d603a6aecb
Noah Bumgardner <noah@playgamecafe.com>
2025-06-11 18:21:44 -0500

    Copy Cannon Base Constructor into Children, Error

--- patterns/random/CannonRandom.gd
+++ patterns/random/CannonRandom.gd
@@ -4,7 +4,11 @@ class_name CannonRandom
 
 func _init(_pattern_parent:Node2D, _pattern_duration:float,
 		_pattern_collision:int, _projectile_infos:Array):
-	pass
+	pattern_parent = _pattern_parent
+	pattern_duration = _pattern_duration
+	pattern_collision = _pattern_collision
+	
+	projectile_infos = _projectile_infos
 
 func populate_velocity(projectile_info:ProjectileInfo):
 	calc_vector = Vector2.RIGHT.rotated(randf() * PI * 2)
