c97a640e13f1294bc22ae6d6cf93fb3e0ba6989a
Noah Bumgardner <noah@playgamecafe.com>
2026-09-10 19:33:11 -0500

    Allow Bullets to Hurt Anyone, Debug Only

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -12,7 +12,7 @@ func _ready():
 func initialize(spawn_position, spawn_velocity, spawn_collision_mask, sprite_frame):
 	position = spawn_position
 	velocity = spawn_velocity
-	collision_layer = 0
+	collision_layer = 1
 	collision_mask = spawn_collision_mask
 	$AnimatedSprite2D.frame = sprite_frame
 	# hacky way to color sprites to indicate player allegiance:
