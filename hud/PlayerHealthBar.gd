99be61cca8345927af802e1d4e5c0b2958260e1e
Noah Bumgarder <noah@playgamecafe.com>
2026-09-11 15:17:04 -0500

    Shrink PR: Minimize Health Bar Code

--- hud/PlayerHealthBar.gd
+++ hud/PlayerHealthBar.gd
@@ -10,12 +10,8 @@ func _ready():
 	max_value = player.max_health
 	_set_player_health_current()
 	player.health_changed.connect(_on_player_health_changed)
-	#if player.player_number == 1:
-		#self_modulate = BLUE
-	#else:
-	#	self_modulate = ORANGE
 
-func _on_player_health_changed(remaining_health: int) -> void:
+func _on_player_health_changed(_remaining_health: int) -> void:
 	_set_player_health_current()
 
 func _set_player_health_current():
