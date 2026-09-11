e2af02b7bca384fbed565977c64446a75c7f69dc
Noah Bumgarder <noah@playgamecafe.com>
2026-09-11 14:13:32 -0500

    Print Player on Collision, Debug

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -33,6 +33,10 @@ func isOutOfBounds():
 		return false
 
 func _on_Bullet_area_shape_entered(_area_id, _area, _area_shape, _self_shape):
+	print(
+		"Bullet hit a player. Bullet parameters are:",
+		_area_id, _area, _area_shape, _self_shape
+	)
 	kill()
 
 func kill():
